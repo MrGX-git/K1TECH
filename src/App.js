08abe012340ff67bfd2419ad0228ab1fc35ccd4d
Mr-1-GX <76249374+Mr-1-GX@users.noreply.github.com>
2022-03-10 18:43:06 +0400

    k1-start

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          K1 Tech DEMO
         </a>
       </header>
     </div>
